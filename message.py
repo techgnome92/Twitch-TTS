548bc6c96f30515197ee879caeb1fddd1444c934
TechGnome <techgnome92@gmail.com>
2025-03-25 01:01:20 -0700

    add say message, tts rate, and voices to message

--- message.py
+++ message.py
@@ -1,7 +1,20 @@
-from twitchAPI.object.eventsub import ChannelChatMessageEvent
+from __twitch.ChannelChatMessage import ChannelChatMessageSourceEvent
 from validate import validate_message, Settings
 from filters import filter_message
 from utils import settings, allowed_users, ignored_users, ignored_words, replace_words, regex_filter
+import os, uuid  # noqa
+from tts import generate_wav
+import simpleaudio as sa
+
+
+voice_options = {
+    "sam": "MSSam",
+    "mike": "MSMike",
+    "mary": "MSMary",
+    "david": "TTS_EN-US_DAVID_11.0",
+    "zira": "TTS_MS_EN-US_ZIRA_11.0",
+    "dectalk": "dectalk"
+}
 
 
 class Message:
@@ -12,17 +25,35 @@ class Message:
     regex_filter: list = regex_filter
     ignored_words: list = ignored_words
     replace_words: dict = replace_words
+    TTS_RUNNING: bool = False
 
-    def __init__(self, data: ChannelChatMessageEvent):
+    def __init__(self, data: ChannelChatMessageSourceEvent):
         self.data = data
+        self.user = data.event.chatter_user_name
         self.message = data.event.message
         self.is_valid = self.validate()
         self.text = self.filter_message()
+        self.rate = self.settings.TTS_RATE
+        self.volume = 100
 
     def validate(self):
-        return validate_message(self.data, self.settings, self.allowed_users, self.ignored_users)
+        if self.TTS_RUNNING:
+            return validate_message(self.data, self.settings, self.allowed_users, self.ignored_users)
+        else:
+            return False
 
     def filter_message(self) -> str:
         if self.is_valid:
             return filter_message(self.message, self.ignored_words, self.replace_words, self.regex_filter)
         return ""
+
+    def say_message(self):
+        session = str(uuid.uuid1())
+        temp_file = f"{session}.wav"
+
+        generate_wav(temp_file, self.text, rate=self.rate, voice=voice_options[self.allowed_users[self.user]])
+
+        wave_obj = sa.WaveObject.from_wave_file(temp_file)
+        play_obj = wave_obj.play()
+        play_obj.wait_done()
+        os.remove(temp_file)
