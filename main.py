2ff31f8eac02e3cc7dc7132624cb015f47ec416c
TechGnome <techgnome92@gmail.com>
2025-04-06 00:24:44 -0700

    remove auto reload from uvicorn

--- main.py
+++ main.py
@@ -149,4 +149,4 @@ def add_regex_filter_row(request: Request):
 if __name__ == "__main__":
     import uvicorn
 
-    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
+    uvicorn.run("main:app", host="0.0.0.0", port=8000)
